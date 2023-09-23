c2e1de67bdf7fb1c3d104a08795dcca27e00929f
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-24 00:50:52 +0530

    input tag added

---
+++ src/components/Header/Banner.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Input, Button } from "@material-tailwind/react";
+
+const Banner = () => {
+  const [text, setText] = React.useState("");
+  const onChange = ({ target }) => setText(target.value);
+
+  return (
+    <div className="bg-deep-purple-100 min-h-[500px]">
+      <div className=" flex justify-center items-center h-[80vh]  max-w-[50rem] mx-auto">
+        <Input
+          type="text"
+          label="Search by Category ..."
+          value={text}
+          onChange={onChange}
+          className=" "
+          containerProps={{
+            className: "min-w-0",
+          }}
+        />
+        <Button
+          size="sm"
+          color={text ? "gray" : "blue-gray"}
+          disabled={!text}
+          className=" rounded px-6 py-3"
+        >
+          Search
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Banner from "../../components/Header/Banner";
 
 const Home = () => {
     return (
         <div>
-            home
+           <Banner></Banner>
         </div>
     );
 };
