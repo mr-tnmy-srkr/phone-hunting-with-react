599e10160672789159d35e108684d992dad79a17
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-24 17:21:45 +0530

    updated

--- src/pages/Favourite/Favourite.jsx
+++ src/pages/Favourite/Favourite.jsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+// import { PhoneCard } from "../Phone/PhoneCard";
+import PhonesCard from "../../components/Phones.jsx/PhonesCard";
+
 const Favourite = () => {
-  return <div>favorite</div>;
+  const [favourites, setFavourites] = useState();
+  const [noFound, setNoFound] = useState("");
+  useEffect(() => {
+    const favouritePhone = JSON.parse(localStorage.getItem("favourites"));
+    if (favouritePhone) {
+      setFavourites(favouritePhone);
+    } else {
+      setNoFound("No Data Found");
+    }
+  }, []);
+  // console.log(favourites);
+  return (
+    <div>
+      {noFound ? (
+        <div className="text-2xl flex items-center justify-center h-[80vh]">
+          {noFound}
+        </div>
+      ) : (
+       <div>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 my-10 ">
+          {
+            favourites?.map(phone=><PhonesCard key={phone.id} phone={phone}></PhonesCard>)
+          }
+        </div>
+       </div>
+      )}
+    </div>
+  );
 };
 
 export default Favourite;
